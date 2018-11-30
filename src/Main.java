d3f7a55311e029d30d96b072fac5c769efdb6316
Ensporet <43299446+Ensporet@users.noreply.github.com>
2018-11-30 15:33:19 +0200

    Twy
    @ensporet

--- src/Main.java
+++ src/Main.java
@@ -12,9 +12,13 @@ public class Main {
                 hobby = Str_Default.Str_Default("Ваше хобби ?") ;
 
         // Первый вариант
+        System.out.println("\n// Первый вариант");
         System.out.printf( "%-20s%s%n%-20s%s%n%-20s%s%n%-20s%s%n","Имя :" ,name, "Город :" ,city, "Возраст :", Age, "Хобби :" ,hobby) ;
 
-
+        //Второй вариант
+        System.out.println("\n//Второй вариант");
+        System.out.println("Человек по имени " + name +" живет в городе " + city +"." +
+        "\nЭтому человеку " + Age +  " лет и любит он заниматься " + " hobby.") ;
 
 
 
